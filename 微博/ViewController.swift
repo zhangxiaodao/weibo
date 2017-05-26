dc2e2dc72d2119dfb10b31ede80b9fada7c7fd90
zhangxiaodao <1547100913@qq.com>
2017-05-26 10:23:34 +0800

    Initial Commit

---
+++ 微博/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  微博
+//
+//  Created by 杭州阿尔法特 on 2017/5/26.
+//  Copyright © 2017年 张海昌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
