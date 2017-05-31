26188078a2ec58940893679cec6e8e1e3500d1cc
zhangxiaodao <1547100913@qq.com>
2017-05-31 10:24:29 +0800

    通过桥接文件  让 Swift 调用  OC的代码

--- 微博/AppDelegate.swift
+++ 微博/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         window?.backgroundColor = UIColor.white
-        window?.rootViewController = WBMainViewController()
+        window?.rootViewController = WBHomeViewController()
         window?.makeKeyAndVisible()
         
         return true
---
+++ 微博/WeiBo-Bridge.h
@@ -0,0 +1,13 @@
+
+//
+//  WeiBo-Bridge.h
+//  微博
+//
+//  Created by 杭州阿尔法特 on 2017/5/31.
+//  Copyright © 2017年 张海昌. All rights reserved.
+//
+
+//桥接文件，专门用于引入 OC 的头文件，一旦引入，Swift就可以正常使用（宏除外）
+
+#import "CZAdditions.h"
+
