864f8ed2792b707bbe4656cad9096e899248cd0c
zhangxiaodao <1547100913@qq.com>
2017-05-31 14:41:38 +0800

    添加撰写按钮

--- 微博/Classes/View(视图和控制器)/Main/WBMainViewController.swift
+++ 微博/Classes/View(视图和控制器)/Main/WBMainViewController.swift
@@ -14,7 +14,11 @@ class WBMainViewController: UITabBarController {
         super.viewDidLoad()
         
         setupChildControllers()
+        setupComposeButton()
     }
+    //MARK - 私有控件
+    /// 撰写按钮
+    lazy var composeButton:UIButton = UIButton.cz_imageButton("tabbar_compose_icon_add", backgroundImageName: "tabbar_compose_button")
 
 }
 
@@ -24,13 +28,26 @@ class WBMainViewController: UITabBarController {
 //注意：和 OC 中的分类一样，extension 不能定义属性
 extension WBMainViewController {
     
+    func setupComposeButton() -> () {
+        tabBar.addSubview(composeButton)
+        
+        //计算按钮的宽度
+        let count = CGFloat(childViewControllers.count)
+        let w = tabBar.bounds.width / count - 1
+        //CGRectInset 正数向内缩进 负数向外扩展
+        composeButton.frame = tabBar.bounds.insetBy(dx: 2 * w, dy: 0)
+        
+        
+    }
+    
     /// 设置所有子控制器
      func setupChildControllers() {
         let array = [
-            ["clsName":"WBHomeViewController" , "title":"首页" , "imageName":""],
-            ["clsName":"WBMessageViewController" , "title":"消息" , "imageName":""],
-            ["clsName":"WBDiscoverViewController" , "title":"发现" , "imageName":""],
-            ["clsName":"WBProfileViewController" , "title":"我的" , "imageName":""],
+            ["clsName":"WBHomeViewController" , "title":"首页" , "imageName":"home"],
+            ["clsName":"WBMessageViewController" , "title":"消息" , "imageName":"message_center"],
+            ["clsName":"ViewController"],
+            ["clsName":"WBDiscoverViewController" , "title":"发现" , "imageName":"discover"],
+            ["clsName":"WBProfileViewController" , "title":"我的" , "imageName":"profile"],
         ]
         var arrayM = [UIViewController]()
         
@@ -58,6 +75,17 @@ extension WBMainViewController {
         //2.创建视图控制器
         let vc = cls.init()
         vc.title = title
+        
+        //3.设置图像
+        vc.tabBarItem.image = UIImage(named: "tabbar_" + imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: "tabbar_" + imageName + "_selected")?.withRenderingMode(.alwaysOriginal)
+        
+        //4.设置 tabbar 的标题颜色
+        vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.orange], for: .highlighted)
+        //设置字体大小
+        //系统默认的是 12 号字体，修改字体大小，要设置 Normal 的字体大小（设置为 .highlighted 无效）
+//        vc.tabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFont(ofSize: 24)], for: UIControlState(rawValue:0))
+        
         let nav = WBNavigationViewController(rootViewController: vc)
         return nav
     }
