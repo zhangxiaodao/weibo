cc2e64ede348883ed43ad2c272b77dbe6cd4e30c
zhangxiaodao <1547100913@qq.com>
2017-06-28 16:35:26 +0800

    第一个 pop 简单动画

--- 微博/Classes/View(视图和控制器)/Compose/ComposeType/WBComposeTypeView.swift
+++ 微博/Classes/View(视图和控制器)/Compose/ComposeType/WBComposeTypeView.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import pop
 
 class WBComposeTypeView: UIView {
     
-    @IBOutlet weak var scrollerView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    /// 返回前一页按钮约束
+    @IBOutlet weak var returnButtonCenterXCons: NSLayoutConstraint!
+    /// 关闭按钮约束
+    @IBOutlet weak var closeButtonCenterXCons: NSLayoutConstraint!
+    
+    @IBOutlet weak var returnButton: UIButton!
+    
     
     //按钮数据数组
-    private let buttonsInfo = [
+    fileprivate let buttonsInfo = [
         ["imageName":"tabbar_compose_idea" , "title":"文字"],
         ["imageName":"tabbar_compose_photo" , "title":"照片/视频"],
         ["imageName":"tabbar_compose_lbs" , "title":"签到"],
         ["imageName":"tabbar_compose_review" , "title":"点评"],
-        ["imageName":"tabbar_compose_more" , "title":"更多"],
+        ["imageName":"tabbar_compose_review" , "title":"长微博"],
+        ["imageName":"tabbar_compose_more" , "title":"更多" , "clickName":"clickMore"],
         ["imageName":"tabbar_compose_friend" , "title":"好友圈"],
         ["imageName":"tabbar_compose_wbcamera" , "title":"微博相机"],
         ["imageName":"tabbar_compose_music" , "title":"音乐"],
@@ -33,6 +43,8 @@ class WBComposeTypeView: UIView {
         //XIB 加载 默认是 600*600
         v.frame = UIScreen.main.bounds
         
+        v.setupUI()
+        
         return v
     }
     
@@ -45,23 +57,146 @@ class WBComposeTypeView: UIView {
         
         //2.添加视图
         vc.view.addSubview(self)
+        showCurrentView()
+    }
+    
+    
+    @objc func clickMore() {
+        print("点击更多")
+        //将 scrollView 滚动到第二页
+        let offset = CGPoint(x: scrollView.bounds.width, y: 0)
+        scrollView.setContentOffset(offset, animated: true)
+        
+        //处理底部按钮,让两个按钮分开
+        returnButton.isHidden = false
+        
+        let margin = scrollView.bounds.width / 6
+        
+        returnButtonCenterXCons.constant -= margin
+        closeButtonCenterXCons.constant += margin
+        
+        UIView.animate(withDuration: 0.25) { 
+            self.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func clickReturn(_ sender: Any) {
+        //1.将滚动视图，滚动到第一页
+        let offset = CGPoint(x: 0, y: 0)
+        scrollView.setContentOffset(offset, animated: true)
+        
+        //2.让两个按钮合并
+        let margin = scrollView.bounds.width / 6
+        returnButtonCenterXCons.constant += margin
+        closeButtonCenterXCons.constant -= margin
+        
+        UIView.animate(withDuration: 0.25, animations: { 
+            self.layoutIfNeeded()
+            self.returnButton.alpha = 0
+        }) { (_) in
+            self.returnButton.isHidden = true
+            self.returnButton.alpha = 1
+        }
         
     }
-    
-    override func awakeFromNib() {
-        setupUI()
-    }
-    
     //关闭视图
     @IBAction func close(_ sender: Any) {
         removeFromSuperview()
     }
 }
 
-extension WBComposeTypeView {
+// MARK: - 动画扩展方法
+fileprivate extension WBComposeTypeView {
+    fileprivate func showCurrentView() {
+        //1>创建动画
+        let anim:POPBasicAnimation = POPBasicAnimation(propertyNamed: kPOPViewAlpha)
+        anim.fromValue = 0
+        anim.toValue = 1
+        anim.duration = 0.5
+        
+        //2>添加到视图
+        pop_add(anim, forKey: nil)
+    }
+}
+
+fileprivate extension WBComposeTypeView {
     
-    func setupUI() -> () {
+    func setupUI() {
+        
+        //0.强行更新布局
+        layoutIfNeeded()
+        
+        //1.向 scrollView 添加视图
+        let rect = scrollView.bounds
+        let width = scrollView.bounds.width
+        
+        for i in 0..<2 {
+            let v = UIView(frame:rect.offsetBy(dx: CGFloat(i) * width, dy: 0))
+            
+            //2.向视图添加按钮
+            addButton(v: v, idx: 6 * i)
+            
+            //3.将试图添加到 scrollView
+            scrollView.addSubview(v)
+        }
+        
+        //4.设置 scrollView 
+        scrollView.contentSize = CGSize(width: 2 * width, height: 0)
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.bounces = false
+        scrollView.isPagingEnabled = true
+        scrollView.isScrollEnabled = false
+    }
+    
+    //向 v 中添加按钮，按钮的数组索引从 idx 开始
+    func addButton(v:UIView , idx:Int)  {
+        let count = 6
+        
+        //从 idx 开始添加 6 个按钮
+        for i in idx..<(idx + count) {
+            
+            if i >= buttonsInfo.count {
+                break
+            }
+            
+            //0.从数组字典中获取图像名称 和 title
+            let dict = buttonsInfo[i]
+            
+            guard let imageName = dict["imageName"],
+                let title = dict["title"]
+                else {
+                    continue
+            }
+            
+            //1>创建按钮
+            let btn = WBComposeTypeButton.composeTypeBtn(imageName: imageName, title: title)
+            v.addSubview(btn)
+            
+            if let actionName = dict["clickName"] {
+                btn.addTarget(self, action: Selector(actionName), for: .touchUpInside)
+            }
+            
+        }
+        
+        //便利视图的子视图，布局按钮
+        let btnSize = CGSize(width: 100, height: 100)
+        let margin = (v.bounds.width - 3 * btnSize.width) / 4
+        
+        for (i , btn) in v.subviews.enumerated() {
+            let y:CGFloat = (i > 2) ? (v.bounds.height - btnSize.height) : 0
+            let col = i % 3
+            let x = CGFloat(col + 1) * margin + CGFloat(col) * btnSize.width
+            
+            btn.frame = CGRect(x: x, y: y, width: btnSize.width, height: btnSize.height)
+            
+        }
         
         
     }
+    
+    
+    
+    
+    
 }
